59333000196032010522b94b224b95ba2feb29f4
Sammynator <sgerules@gmail.com>
2025-02-17 17:32:21 +0100

    added all functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  pancakeImage: string = 'url(/not-burned.png)'; 
+  isFlipping: boolean = false; // Flag to trigger the flip animation
 
+  flipPancake() {
+    this.isFlipping = true;
+
+    setTimeout(() => {
+      // After the flip animation ends, randomly choose between heads or tails
+      this.pancakeImage = Math.random() < 0.5 ? 'url(/not-burned.png)' : 'url(/burned.png)';
+      this.isFlipping = false; // End flip animation
+    }, 1000); // Duration of the flip animation (1 second)
+  }
 }
